482db0f677932e051be982057c72f39157410f2b
ultron07 <50156573+ultron07@users.noreply.github.com>
2021-03-19 16:32:43 +0530

    feat: created index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+var readlineSync = require('readline-sync');
+
+
+
+var score = 0;
+function play(question, answer) {
+  var userAnswer = readlineSync.question(question)
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) {
+    console.log("U r right!!!")
+    score++
+  } else {
+    console.log("U r wrong!!")
+  }
+  console.log("Your score is "
+    + score)
+}
+
+questionOne = {
+  question: "Where I live?",
+  answer: "Agra"
+}
+
+questionTwo = {
+  question: "What is my gaming name?",
+  answer: "Ultron"
+}
+
+questionThree = {
+  question: "Who is my favorite villian?",
+  answer: "Thanos"
+}
+
+questionFour = {
+  question: "Who is my favorite superhero?",
+  answer: "Ironman"
+}
+
+questionFive = {
+  question: "Which is my favorite sad song?",
+  answer: "Happier"
+}
+
+var question = [questionOne, questionTwo, questionThree, questionFour, questionFive]
+
+
+for (var i = 0; i < question.length; i++) {
+  play(question[i].question, question[i].answer)
+}
+
+console.log("YAY u completed the game.Final Score is " + score)
